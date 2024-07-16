49727d09fa8a63b01c709ca6103ac919757c99cd
Your Name <you@example.com>
2024-07-16 10:51:18 +0700

    push Bt5

---
+++ src/session5/run/Main.java
@@ -0,0 +1,8 @@
+package session5.run;
+import session5.run.Management;
+public class Main {
+    public static void main(String[] args) {
+        Management shopManagement = new Management();
+        shopManagement.shopMenu();
+    }
+}
